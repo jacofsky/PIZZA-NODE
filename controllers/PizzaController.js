7cf292fc4ec17735d32aec7d2a93fe786af8d36a
Jaco <Jaco>
2022-05-31 10:47:09 -0300

    Terminado

--- controllers/PizzaController.js
+++ controllers/PizzaController.js
@@ -1,6 +1,7 @@
 
 import {request, response} from 'express'
-import { selectPizzas } from '../services/pizzas-services.js'
+import Pizza from '../models/Pizza.js'
+import { createPizza, deleteById, selectPizzaById, selectPizzas, updatePizza } from '../services/pizzas-services.js'
 
 export const getPizzas = async(req = request, res = response) => {
 
@@ -8,9 +9,101 @@ export const getPizzas = async(req = request, res = response) => {
     try {
         const pizzas = await selectPizzas()
 
-        res.status(200).json(pizzas)
+        res.status(200).json({
+            count: pizzas.rowsAffected[0],
+            pizzas: pizzas.recordset
+        })
         
     } catch (error) {
+        res.status(500).json({msg: 'Algo salio mal 😮'})
+    }
+
+}
+
+export const getPizzasById = async(req = request, res = response) => {
+
+    
+    try {
+
+        const {id} = req.params
+
+        const pizzas = await selectPizzaById(id)
+
+        res.status(200).json(pizzas.recordset[0])
+        
+    } catch (error) {
+        res.status(500).json({msg: 'Algo salio mal 😮'})
+    }
+
+}
+
+export const postPizza = async(req = request, res = response) => {
+
+    
+    try {
+
+        const {nombre, libreGluten, importe, descripcion} = req.body
+
+        const pizza = new Pizza(nombre, libreGluten, importe, descripcion)
+
+        const pizzaCreada = await createPizza(pizza)
+
+        res.status(200).json(pizzaCreada)
+        
+    } catch (error) {
+        res.status(500).json({msg: 'Algo salio mal 😮'})
+    }
+
+}
+
+
+export const updPizza = async(req = request, res = response) => {
+
+    
+    try {
+
+        const {id} = req.params
+
+        const {nombre, libreGluten, importe, descripcion} = req.body
+        
+        const pizza = new Pizza(nombre, libreGluten, importe, descripcion)
+
+        const pizzas = await updatePizza(pizza, id)
+
+        if (pizzas.rowsAffected[0] === 0) {
+            return res.status(404).json(`Pizza numero ${id} no encontrada`)
+        }
+
+        res.status(200).json(`Pizza numero ${id} actualizada`)
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg: 'Algo salio mal 😮'})
+    }
+
+}
+
+export const deletePizza = async(req = request, res = response) => {
+
+    
+    try {
+
+        const {id} = req.params
+
+        const pizzas = await deleteById(id)
+
+        console.log(pizzas.rowsAffected[0]);
+
+        if (pizzas.rowsAffected[0] === 0) {
+            console.log("Hla");
+            return res.status(404).json(`Pizza numero ${id} no encontrada`)
+            
+        }
+
+        res.status(200).json(`Pizza numero ${id} eliminada`)
+        
+    } catch (error) {
+        console.log(error);
         res.status(500).json({msg: 'Algo salio mal 😮'})
     }
 
--- routes/PizzaRoutes.js
+++ routes/PizzaRoutes.js
@@ -1,12 +1,41 @@
 
 
 import Router from 'express'
-import { getPizzas } from '../controllers/PizzaController.js'
+import { check } from 'express-validator'
+import { deletePizza, getPizzas, getPizzasById, postPizza, updPizza } from '../controllers/PizzaController.js'
+import fieldValidation from '../middlewares/fieldValidation.js'
 
 const route = Router()
 
 
 route.get('/', getPizzas)
+
+route.get('/:id',
+    check('id', "El id no es valido").isNumeric(),
+    fieldValidation
+, getPizzasById)
+
+route.post('/',
+    check('nombre', "El id no es valido").isString(),
+    check('libreGluten', "Libre de gluten debe ser un bool").isBoolean(),
+    check('importe', "El importe no es valido").isNumeric(),
+    check('descripcion', "La descripcion no es valida").isString(),
+    fieldValidation
+, postPizza)
+
+route.put('/:id',
+    check('nombre', "El id no es valido").isString(),
+    check('libreGluten', "Libre de gluten debe ser un bool").isBoolean(),
+    check('importe', "El importe no es valido").isNumeric(),
+    check('descripcion', "La descripcion no es valida").isString(),
+    check('id', "El id no es valido").isNumeric(),
+    fieldValidation
+, updPizza)
+
+route.delete('/:id',
+    check('id', "El id no es valido").isNumeric(),
+    fieldValidation
+, deletePizza)
 
 export default route
 
